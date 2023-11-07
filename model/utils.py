b636817eba08cae9942e603c5fa50689d9f5675e
Daniil Tolstykh <daniil.tolstykh.1996@gmail.com>
2023-11-07 13:54:44 +0400

    refactored code

--- model/utils.py
+++ model/utils.py
@@ -8,7 +8,7 @@ def xywh2box(boxes: np.ndarray, scale: float, padw: float, padh: float) -> np.nd
     boxes[..., [0, 1]] -= np.array([padw, padh])
     boxes /= scale
 
-    return boxes.astype(np.uint16)
+    return boxes
 
 
 def xywh2xyxy(boxes: np.ndarray) -> np.ndarray:
@@ -22,12 +22,11 @@ def xyxy2new_shape(boxes: np.ndarray, old_shape, new_shape) -> np.ndarray:
     """Convert box coords (x1,y1,x2,y2) to the new shape"""
     h_old, w_old = old_shape
     h_new, w_new = new_shape
-    boxes = boxes.astype(np.float32)
 
     boxes[..., [0, 2]] *= w_new / w_old
     boxes[..., [1, 3]] *= h_new / h_old
 
-    return boxes.astype(np.uint16)
+    return boxes
 
 
 def crop_mask(masks: np.ndarray, boxes: np.ndarray) -> np.ndarray:
@@ -168,7 +167,7 @@ def process_masks(
     def sigmoid(x: np.ndarray) -> np.ndarray:
         return 1.0 / (1.0 + np.exp(-x))
 
-    masks = sigmoid(masks_in @ protos.view().reshape(c, -1)).view().reshape(-1, mh, mw)  # (n, mh, mw)
+    masks = sigmoid(masks_in @ protos.reshape(c, -1)).reshape(-1, mh, mw)  # (n, mh, mw)
 
     gain = min(mh / input_size[0], mw / input_size[1])  # 0.25 predefined by yolo
     padw, padh = pad[0] * gain, pad[1] * gain
@@ -184,12 +183,10 @@ def process_masks(
     else:
         # upsample masks to shape (model_height - 2*padh, model_width - 2*padw)
         up_w, up_h = map(round, [(right - left) / gain, (bottom - top) / gain])
-
-    boxes_scaled = boxes.copy()
-    boxes_scaled = xyxy2new_shape(boxes_scaled, original_img_shape, (up_h, up_w))
+        boxes = xyxy2new_shape(boxes.copy(), original_img_shape, (up_h, up_w))
 
     masks = cv2.resize(masks, dsize=(up_w, up_h), interpolation=cv2.INTER_LINEAR)  # (up_h, up_w, n)
     masks = masks[..., None] if len(masks.shape) == 2 else masks
-    masks = crop_mask(masks, boxes_scaled)  # (up_h, up_w, n)
+    masks = crop_mask(masks, boxes)  # (up_h, up_w, n)
 
     return masks
