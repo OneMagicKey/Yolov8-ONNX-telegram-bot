b6eeded3412d29d3dee8fb5ac899d3dae1f0cc70
Daniil Tolstykh <daniil.tolstykh.1996@gmail.com>
2024-02-15 17:16:59 +0400

    add tests

---
+++ tests/test_bot.py
@@ -0,0 +1,19 @@
+import os
+import unittest
+from src.utils import init_models
+
+
+class BotTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        dummy_token = "1234567890:ABCDEfgh-q1wERT2Yuiopasdfghjklzxc3v"
+        os.environ["TELEGRAM_TOKEN"] = dummy_token
+
+    def test_model_initialization(self):
+        from bot import model_list
+
+        self.assertIsInstance(init_models(model_list), dict)
+
+
+if __name__ == "__main__":
+    unittest.main()
